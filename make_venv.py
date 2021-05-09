6b53ff327160d486fc4b9ee7f85d855ec72384f9
zackees <z@zackees.com>
2021-05-09 10:55:38 -0700

    Update make_venv.py

--- make_venv.py
+++ make_venv.py
@@ -4,15 +4,14 @@
   cd <YOUR DIRECTORY>
   Download and install in one line:
     curl -X GET https://raw.githubusercontent.com/zackees/make_venv/main/make_venv.py | python
-  Then :
-    . activate.sh
+
+  To enter the environment run:
+    source activate.sh
 
   Notes:
     This script is tested to work using python2 and python3 from a fresh install. The only side effect
     of running this script is that virtualenv will be globally installed if it isn't already.
 """
-
-
 
 import os
 import sys
