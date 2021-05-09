d0a9c47751a464bffb15a860b542b58b18238b79
zackees <z@zackees.com>
2021-05-09 10:31:05 -0700

    Update make_venv.py

--- make_venv.py
+++ make_venv.py
@@ -4,7 +4,7 @@
 ## Quick install
 # cd <YOUR DIRECTORY>
 # Download and install in one line:
-#   curl -X GET https://raw.githubusercontent.com/zackees/make_venv/main/make_venv.py -o make_env.py | python
+#   curl -X GET https://raw.githubusercontent.com/zackees/make_venv/main/make_venv.py | python
 # Then :
 #   . activate.sh
 
