8c1fc0613845dd0bb09a16fb3035ad8ecc1f09d2
zackees <z@zackees.com>
2021-05-09 10:54:20 -0700

    Update make_venv.py

--- make_venv.py
+++ make_venv.py
@@ -1,17 +1,25 @@
 
+"""
+  Quick install
+  cd <YOUR DIRECTORY>
+  Download and install in one line:
+    curl -X GET https://raw.githubusercontent.com/zackees/make_venv/main/make_venv.py | python
+  Then :
+    . activate.sh
+
+  Notes:
+    This script is tested to work using python2 and python3 from a fresh install. The only side effect
+    of running this script is that virtualenv will be globally installed if it isn't already.
+"""
 
 
-## Quick install
-# cd <YOUR DIRECTORY>
-# Download and install in one line:
-#   curl -X GET https://raw.githubusercontent.com/zackees/make_venv/main/make_venv.py | python
-# Then :
-#   . activate.sh
 
 import os
 import sys
 import subprocess
 
+# This activation script adds the ability to run it from any path and also
+# aliasing pip3 and python3 to pip/python so that this works across devices.
 _ACTIVATE_SH = """
 function abs_path {
   (cd "$(dirname '$1')" &>/dev/null && printf "%s/%s" "$PWD" "${1##*/}")
@@ -35,7 +43,6 @@ def is_tool(name):
     """Check whether `name` is on PATH."""
     from distutils.spawn import find_executable
     return find_executable(name) is not None
-
 
 if not os.path.exists('venv'):
     if not is_tool('virtualenv'):
