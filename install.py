1adbd6d36842ad469c10ef3297f9f5148565242a
Zachary Vorhies <zachvorhies@protonmail.com>
2023-08-04 14:58:50 -0700

    Update install.py

--- install.py
+++ install.py
@@ -3,7 +3,7 @@
   Quick install
   cd <YOUR DIRECTORY>
   Download and install in one line:
-    curl -X GET https://raw.githubusercontent.com/zackees/make_venv/main/make_venv.py | python
+    curl -X GET https://raw.githubusercontent.com/zackees/make_venv/main/install.py | python
 
 
   To enter the environment run:
