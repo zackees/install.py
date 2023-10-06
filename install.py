9cd1d6017d16fc259466e4497eaf5bfc2beda8b1
Zachary Vorhies <zachvorhies@protonmail.com>
2023-10-05 19:07:25 -0700

    Update install.py

--- install.py
+++ install.py
@@ -17,14 +17,15 @@
 
 
 import os
-import sys
 import subprocess
-
+import sys
+import argparse
+import shutil
 
 # This activation script adds the ability to run it from any path and also
 # aliasing pip3 and python3 to pip/python so that this works across devices.
 _ACTIVATE_SH = """
-
+#!/bin/bash
 
 # Function that computes absolute path of a file
 function abs_path {
@@ -32,7 +33,6 @@ function abs_path {
   # the present working directory followed by the base name of the file
   (cd "$(dirname '$1')" &>/dev/null && printf "%s/%s" "$PWD" "${1##*/}")
 }
-
 
 # Navigate to the directory where the current script resides
 cd $( dirname $(abs_path ${BASH_SOURCE[0]}))
@@ -45,8 +45,6 @@ else
   # Otherwise, set 'IN_ACTIVATED_ENV' to false
   IN_ACTIVATED_ENV=false
 fi
-
-
 
 # If the 'venv' directory doesn't exist, print a message and exit.
 if [[ ! -d "venv" ]]; then
@@ -65,7 +63,6 @@ if [[ ! -d "venv" ]]; then
     python install.py
   fi
 
-
   . ./venv/bin/activate
   export IN_ACTIVATED_ENV=1
   export PATH=$( dirname $(abs_path ${BASH_SOURCE[0]}))/:$PATH
@@ -74,30 +71,27 @@ if [[ ! -d "venv" ]]; then
   exit 0
 fi
 
-
 # Activate the Python virtual environment. 
 # The environment must be created beforehand and exist in a directory named 'venv' 
 # in the same directory as this script.
 . ./venv/bin/activate
 
-
 # Add the directory where the current script resides to the PATH variable. 
 # This allows executing files from that directory without specifying the full path.
 export PATH=$( dirname $(abs_path ${BASH_SOURCE[0]}))/:$PATH
 """
-
-
 HERE = os.path.dirname(__file__)
 os.chdir(os.path.abspath(HERE))
 
-IN_ACTIVATED_ENV = os.environ.get('IN_ACTIVATED_ENV', '0') == '1'
-if IN_ACTIVATED_ENV:
-    print("Cannot install a new environment while in an activated environment. Please launch a new shell and try again.")
-    sys.exit(1)
-
-
 def _exe(cmd):
-    print('Executing "%s"' % cmd)
+    msg = (
+        "########################################\n"
+        f"# Executing '{cmd}'\n"
+        "########################################\n"
+    )
+    print(msg)
+    sys.stdout.flush()
+    sys.stderr.flush()
     #os.system(cmd)
     subprocess.check_call(cmd, shell=True)
     
@@ -106,8 +100,7 @@ def is_tool(name):
     from distutils.spawn import find_executable
     return find_executable(name) is not None
 
-
-if not os.path.exists('venv'):
+def create_virtual_environment() -> None:
     if not is_tool('virtualenv'):
         _exe('pip install virtualenv')
     # Which one is better? virtualenv or venv? This may switch later.
@@ -124,10 +117,31 @@ if not os.path.exists('venv'):
         fd.write(_ACTIVATE_SH)
     if sys.platform != "win32":
         _exe('chmod +x activate.sh')
-    pip = 'pip3' if sys.platform == 'win32' else 'pip'
-    _exe(f". ./activate.sh && {pip} install -e .")
-else:
-    print(f'{os.path.abspath("venv")} already exists')
 
+def main() -> int:
+    IN_ACTIVATED_ENV = os.environ.get('IN_ACTIVATED_ENV', '0') == '1'
+    if IN_ACTIVATED_ENV:
+        print("Cannot install a new environment while in an activated environment. Please launch a new shell and try again.")
+        return 1
+    
+    parser = argparse.ArgumentParser(description='Install the project.')
+    parser.add_argument('--remove', action='store_true', help='Remove the virtual environment')
+    args = parser.parse_args()
+    if args.remove:
+        print('Removing virtual environment')
+        shutil.rmtree('venv', ignore_errors=True)
+        node_modules = os.path.join('www', 'node_modules')
+        print(f'Removing {node_modules}')
+        shutil.rmtree(node_modules, ignore_errors=True)
+        return 0
+    if not os.path.exists('venv'):
+        create_virtual_environment()
+    else:
+        print(f'{os.path.abspath("venv")} already exists')
+    _exe(f"bash activate.sh && pip install -e .")
 
-print('Now use ". activate.sh" (at the project root dir) to enter into the environment.')
+    print('Now use ". activate.sh" (at the project root dir) to enter into the environment.')
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
