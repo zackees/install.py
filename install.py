fd13f1edd0bbc88bbc3b43f0c28f25df0bab3cc0
zackees <z@zackees.com>
2023-10-24 19:13:00 -0700

    update

--- install.py
+++ install.py
@@ -29,36 +29,28 @@ _ACTIVATE_SH = """
 
 # Function that computes absolute path of a file
 abs_path() {
-  # Navigate to the directory of the given file (silencing output) and then print
-  # the present working directory followed by the base name of the file
   dir=$(dirname "$1")
   (cd "$dir" &>/dev/null && printf "%s/%s" "$PWD" "${1##*/}")
 }
 
 # Navigate to the directory where the current script resides
-bashfile=$(abs_path ${BASH_SOURCE[0]})
+bashfile=$(abs_path "${BASH_SOURCE[0]}")
 selfdir=$(dirname "$bashfile")
 cd "$selfdir"
 
-
 if [[ "$IN_ACTIVATED_ENV" == "1" ]]; then
-  # If it is, set the variable 'IN_ACTIVATED_ENV' to true
   IN_ACTIVATED_ENV=true
 else
-  # Otherwise, set 'IN_ACTIVATED_ENV' to false
   IN_ACTIVATED_ENV=false
 fi
 
-# If the 'venv' directory doesn't exist, print a message and exit.
 if [[ ! -d "venv" ]]; then
   echo "The 'venv' directory does not exist, creating..."
-  if [[ "$IN_ACTIVATED_ENV" == "1" ]]; thenf
+  if [[ "$IN_ACTIVATED_ENV" == "1" ]]; then   # Fixed typo here
     echo "Cannot install a new environment while in an activated environment. Please launch a new shell and try again."
     exit 1
   fi
-  # Check the operating system type.
-  # If it is macOS or Linux, then create an alias 'python' for 'python3'
-  # and an alias 'pip' for 'pip3'. This is helpful if python2 is the default python in the system.
+  
   echo "OSTYPE: $OSTYPE"
   if [[ "$OSTYPE" == "darwin"* || "$OSTYPE" == "linux-gnu"* ]]; then
     python3 install.py
@@ -68,20 +60,14 @@ if [[ ! -d "venv" ]]; then
 
   . ./venv/bin/activate
   export IN_ACTIVATED_ENV=1
-  export PATH=$selfdir/:$PATH
+  export PATH="$selfdir/:$PATH"
   echo "Environment created."
   pip install -e .
   exit 0
 fi
 
-# Activate the Python virtual environment. 
-# The environment must be created beforehand and exist in a directory named 'venv' 
-# in the same directory as this script.
 . ./venv/bin/activate
-
-# Add the directory where the current script resides to the PATH variable. 
-# This allows executing files from that directory without specifying the full path.
-export PATH=$( dirname $(selfdir)/:$PATH
+export PATH="$selfdir/:$PATH"
 """
 HERE = os.path.dirname(__file__)
 os.chdir(os.path.abspath(HERE))
